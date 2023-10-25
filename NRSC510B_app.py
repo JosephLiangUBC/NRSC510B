da6bfae5479fbedcecdc764f410ab0d6d8a2c0d5
Joseph Liang <joseph90003@gmail.com>
2023-10-25 10:56:42 -0700

    More progress

--- NRSC510B_app.py
+++ NRSC510B_app.py
@@ -15,7 +15,7 @@ def read(table):
 # Read data from SQLite database
 conn = sqlite3.connect('/Users/Joseph/Desktop/NRSC510B/mwt_data.db')
 tap_output = read('tap_response_data')
-tap_baseline = read('tap_baseline_data')
+# tap_baseline = read('tap_baseline_data')
 tap_tstat_allele = read('tstat_data')
 allele_metric_data=read('allele_phenotype_data')
 
@@ -91,6 +91,16 @@ gene_option = st.selectbox(
     'Select a gene',
     (tap_output['Gene'].unique()))
 
+
+tap_output_gene=tap_output[tap_output['Gene']==gene_option]
+# st.write(tap_output_allele)
+# st.write(tap_output_allele['Date'].unique())
+gene_tap_data=tap_output[tap_output['Date'].isin(tap_output_gene['Date'].unique())]
+gene_tap_data_plot=gene_tap_data[gene_tap_data['Gene'].isin(['N2', gene_option])]
+gene_tap_data_plot['taps']=gene_tap_data_plot['taps'].astype(int)
+st.write(gene_tap_data_plot)
+
+
 col3, col4 = st.columns([1,1])
 col3.subheader('phenotypic profile')
 # seaborn graph of phenotypic profile of selected gene
@@ -111,29 +121,91 @@ tab1, tab2, tab3 = st.tabs(["Probability of Tap-Response",
                             "Speed of Tap-Response"])
 
 with tab1:
+   #  Habituation of Response Probability Plot
    st.subheader("Probability")
    fig, ax = plt.subplots(figsize=(12, 10))
    # seaborn plot
+   plt.gca().xaxis.grid(False)  # <- gets rid of x-axis markers to make data look clean
+   ax = sns.pointplot(x="taps",  # <- Here we use seaborn as our graphing package.
+                      y="prob",
+                      data=gene_tap_data_plot,
+                      hue='Gene',  # <- Here we use the extra column from step 6 to separate by group
+                      errorbar='se')  # <- Confidence interval. 95 = standard error
+   plt.xlabel("Taps")  # <- X-axis title
+   plt.ylabel("Probability")  # <- Y-Axis title
+   plt.title("Habituation of Response Probability", fontsize='16')  # <- Figure Title
+   plt.ylim(0, 1)
+   ax.legend(loc='upper right', fontsize='12')  # <- location of your legend
+
    tab1.pyplot(fig)
    tab1.caption(f'Habituation of Response Probability: {gene_option}')
-# Seaborn graph of probability habituation curve
-# Insert download graph button
+
+   # download graph button
+   img1 = io.BytesIO()
+   plt.savefig(img1, format='png', dpi=300, bbox_inches='tight')
+   st.download_button(label="Download Plot",
+                      data=img1,
+                      file_name=f"Probability of Tap Habituation {gene_option}.png",
+                      mime="image/png",
+                      key='dnldbtn1')
+
 
 with tab2:
+   #  Habituation of Response Duration Plot
    st.subheader("Duration")
    fig, ax = plt.subplots(figsize=(12, 10))
    # seaborn plot
+   ax = sns.pointplot(x="taps",
+                      y="dura",
+                      data=gene_tap_data_plot,
+                      hue='Gene',
+                      # palette=pal,
+                      errorbar='se')
+   plt.xlabel("Taps", fontsize='12')
+   plt.ylabel("Duration", fontsize='12')
+   plt.title("Habituation of Response Duration", fontsize='16')
+   plt.ylim(0, None)
+   ax.legend(loc='upper right', fontsize='12')
    tab2.pyplot(fig)
    tab2.caption(f'Habituation of Response Duration: {gene_option}')
+
+   # download graph button
+   img2 = io.BytesIO()
+   plt.savefig(img2, format='png', dpi=300, bbox_inches='tight')
+   st.download_button(label="Download Plot",
+                      data=img2,
+                      file_name=f"Duration of Tap Habituation {gene_option}.png",
+                      mime="image/png",
+                      key='dnldbtn2')
 # Seaborn Graph of Duration Habituation curve
 # Insert download graph button
 
 with tab3:
+   #  Habituation of Response Speed Plot
    st.subheader("Speed")
    fig, ax = plt.subplots(figsize=(12, 10))
    # seaborn plot
+   ax = sns.pointplot(x="taps",
+                      y="speed",
+                      data=gene_tap_data_plot,
+                      hue='Gene',
+                      errorbar='se')
+   plt.xlabel("Taps", fontsize='12')
+   plt.ylabel("Speed", fontsize='12')
+   plt.title("Habituation of Response Speed", fontsize='16')
+   plt.ylim(0, None)
+   ax.legend(loc='upper right', fontsize='12')
    tab3.pyplot(fig)
    tab3.caption(f'Habituation of Response Speed: {gene_option}')
+
+   # download graph button
+   img3 = io.BytesIO()
+   plt.savefig(img3, format='png', dpi=300, bbox_inches='tight')
+   st.download_button(label="Download Plot",
+                      data=img3,
+                      file_name=f"Speed of Tap Habituation {gene_option}.png",
+                      mime="image/png",
+                      key='dnldbtn3')
 # seaborn graph of Speed Habituation Curve
 # Insert download graph button
 
@@ -174,6 +246,7 @@ tab4, tab5, tab6 = st.tabs(["Probability of Tap-Response",
                             "Speed of Tap-Response"])
 
 with tab4:
+   #  Habituation of Response Probability Plot
    st.subheader("Probability")
    fig, ax = plt.subplots(figsize=(12, 10), linewidth=2.5)
    # seaborn plot
@@ -185,25 +258,24 @@ with tab4:
                       errorbar='se')  # <- Confidence interval. 95 = standard error
    plt.xlabel("Taps")  # <- X-axis title
    plt.ylabel("Probability")  # <- Y-Axis title
-   plt.title("Probability of Tap Habituation", fontsize='16')  # <- Figure Title
+   plt.title("Habituation of Response Probability", fontsize='16')  # <- Figure Title
    plt.ylim(0, 1)
    ax.legend(loc='upper right', fontsize='12')  # <- location of your legend
 
    tab4.pyplot(fig)
    tab4.caption(f'Habituation of Response Probability: {allele_option}')
 
-
-   img = io.BytesIO()
-   plt.savefig(img, format='png', dpi=300, bbox_inches='tight')
+   # download graph button
+   img4 = io.BytesIO()
+   plt.savefig(img4, format='png', dpi=300, bbox_inches='tight')
    st.download_button(label="Download Plot",
-                      data = img,
-                      file_name=f"Probability of Tap Habituation, {allele_option}",
+                      data = img4,
+                      file_name=f"Probability of Tap Habituation {allele_option}.png",
                       mime="image/png",
                       key='dnldbtn4')
-# Seaborn graph of probability habituation curve
-# Insert download graph button
 
 with tab5:
+   #  Habituation of Response Duration Plot
    st.subheader("Duration")
    fig, ax = plt.subplots(figsize=(12, 10))
    # seaborn plot
@@ -221,17 +293,18 @@ with tab5:
    tab5.pyplot(fig)
    tab5.caption(f'Habituation of Response Duration: {allele_option}')
 
-   img = io.BytesIO()
-   plt.savefig(img, format='png', dpi=300, bbox_inches='tight')
+   # download graph button
+   img5 = io.BytesIO()
+   plt.savefig(img5, format='png', dpi=300, bbox_inches='tight')
    st.download_button(label="Download Plot",
-                      data = img,
-                      file_name=f"Probability of Tap Habituation, {allele_option}",
+                      data = img5,
+                      file_name=f"Duration of Tap Habituation {allele_option}.png",
                       mime="image/png",
                       key='dnldbtn5')
-# Seaborn Graph of Duration Habituation curve
-# Insert download graph button
+
 
 with tab6:
+   #  Habituation of Response Speed Plot
    st.subheader("Speed")
    fig, ax = plt.subplots(figsize=(12, 10))
    # seaborn plot
@@ -248,15 +321,15 @@ with tab6:
    tab6.pyplot(fig)
    tab6.caption(f'Habituation of Response Speed: {allele_option}')
 
-   img = io.BytesIO()
-   plt.savefig(img, format='png', dpi=300, bbox_inches='tight')
+   # download graph button
+   img6 = io.BytesIO()
+   plt.savefig(img6, format='png', dpi=300, bbox_inches='tight')
    st.download_button(label="Download Plot",
-                      data = img,
-                      file_name=f"Probability of Tap Habituation, {allele_option}",
+                      data = img6,
+                      file_name=f"Speed of Tap Habituation {allele_option}.png",
                       mime="image/png",
                       key='dnldbtn6')
-# seaborn graph of Speed Habituation Curve
-# Insert download graph button
+
 
 
 
